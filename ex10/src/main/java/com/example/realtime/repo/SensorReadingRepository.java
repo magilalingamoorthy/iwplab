29f460d647bdaf640eb73e4a1e0119a31a613c22
magilalingamoorthy <magilalingamoorthy@gmail.com>
2026-09-24 14:15:37 +0530

    Add files via upload

---
+++ ex10/src/main/java/com/example/realtime/repo/SensorReadingRepository.java
@@ -0,0 +1,8 @@
+package com.example.realtime.repo; 
+ 
+import com.example.realtime.model.SensorReading; 
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository; 
+ 
+public interface SensorReadingRepository extends 
+ReactiveMongoRepository<SensorReading, String> {  
+}
